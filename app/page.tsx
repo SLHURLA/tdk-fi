661d43f8364c3ac50019a3425e4122cb2eba9272
SLHURLA <helpline.steploops@gmail.com>
2025-05-06 19:00:20 +0530

    Fix:build

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { getServerSession } from "next-auth";
 export default async function Home() {
   const session = await getServerSession();
   console.log(JSON.stringify(session));
-  // test
   return (
     <div className="h-screen w-full flex justify-center items-center text-5xl">
       COMING SOON
