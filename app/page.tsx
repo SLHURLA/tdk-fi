ee7061f69c9e2a4cb5db3a5705b6a97869299c7a
SLHURLA <helpline.steploops@gmail.com>
2026-01-08 09:08:26 +0530

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { getServerSession } from "next-auth";
-//fix
+import { redirect } from "next/navigation";
+
 export default async function Home() {
   const session = await getServerSession();
-  console.log(JSON.stringify(session));
+  
+  if (session) {
+    redirect('/tsmgowp/');
+  }
+  
   return (
-    <div className="h-screen w-full flex justify-center items-center text-5xl">
-      COMING SOON
+    <div>
+      {/* Your actual homepage content here */}
+      <h1>Welcome to Your App</h1>
     </div>
   );
 }
